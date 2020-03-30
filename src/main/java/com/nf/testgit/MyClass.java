852bd9f04074d843848ba8034b57c15d3029ce37
yaonengchao <yaonengchao@vip.qq.com>
2020-03-30 21:32:10 +0800

    粉色的发生发的

--- src/main/java/com/nf/testgit/MyClass.java
+++ src/main/java/com/nf/testgit/MyClass.java
@@ -2,6 +2,6 @@ package com.nf.testgit;
 
 public class MyClass {
 	public static void main(String[] args) {
-		System.out.println("ÄãºÃmeihao");
+		System.out.println("ÄãºÃmeihaosdffËÆ¶®·Ç¶®sa");
 	}
 }
